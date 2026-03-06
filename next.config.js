5198866838e156920898a2f5f2ad9316a7814666
joycube <joycube@gmail.com>
2026-03-06 14:14:27 +0900

    1234

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['undici'],
-  // 🔥 [이미지 최적화 설정 추가] 외부 도메인에서 가져오는 이미지를 Next.js 서버가 압축할 수 있도록 허용
+  // 🔥 Next.js Image 컴포넌트 외부 도메인 허용 화이트리스트
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // 모든 https 이미지 허용
-      },
-      {
-        protocol: 'http',
-        hostname: '**', // 모든 http 이미지 허용
-      },
+      { protocol: 'https', hostname: 'lootlevelchill.com' }, // 방금 에러난 배너 사이트
+      { protocol: 'https', hostname: 'www.konami.com' },     // 코나미 배너
+      { protocol: 'https', hostname: 'img.konami.com' },     // 코나미 이미지
+      { protocol: 'https', hostname: 'i.ytimg.com' },        // 유튜브 썸네일
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' }, // 구글 프로필 사진
+      { protocol: 'https', hostname: 'i.pinimg.com' },       // 핀터레스트 (불타는 베컴)
+      { protocol: 'https', hostname: 'stickershop.line-scdn.net' }, // 라인 스티커
+      { protocol: 'https', hostname: '**' },                 // 최신 Next.js 버전용 범용 허용
     ],
   },
 };
+
 module.exports = nextConfig;
